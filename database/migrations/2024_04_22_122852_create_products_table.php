0713785511677a2a0ac7726e5fcb1ad6d154fdf9
SilverMoonDev <jcarcelle03@gmail.com>
2024-04-22 16:22:33 +0200

    made and migrated the table, Added the School logo and removed the hyperlink from the logo

---
+++ database/migrations/2024_04_22_122852_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('cas_number');
+            $table->string('name');
+            $table->decimal('concentration', 5, 2)->nullable();
+            $table->integer('ml');
+            $table->date('expire_date')->nullable();
+            $table->string('cabinet');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
