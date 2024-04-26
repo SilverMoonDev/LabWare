16e7de465a265104c0029f25c1d1d06dbb9e5098
SilverMoonDev <jcarcelle03@gmail.com>
2024-04-26 18:58:00 +0200

    Fixed routes and redirected to login if the user is not logged in

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,28 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Main', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::get('/create', function () {
-    return Inertia::render('Create');
-});
-
-Route::get('/update', function () {
-    return Inertia::render('Update');
-});
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return redirect()->route('products.index');
+    });
+
+    Route::get('/update', function () {
+        return Inertia::render('Update');
+    });
+
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->middleware(['verified'])->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('products', ProductController::class);
 });
 
 require __DIR__.'/auth.php';
+
